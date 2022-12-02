c6cff9a75e9e187e63ef9e0b5748f259f3b6e3e3
Mindmech <alex8008@bk.ru>
2022-12-02 08:24:16 +0300

    HW2 task1 done

---
+++ hw2/t1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message}>");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+int inputN = Prompt("Введите трёхзначное число");
+
+if (inputN >= 100 && inputN < 1000)
+{
+    int seconddigit = (inputN % 100) / 10;
+    Console.WriteLine(seconddigit);
+}
+else    
+Console.WriteLine($"Число не трёхзначное");
