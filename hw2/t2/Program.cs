2427d91c4feb28432ca45e268916db4ab1ad0c8b
Mindmech <alex8008@bk.ru>
2022-12-02 08:33:10 +0300

    HW2 task 2 done

---
+++ hw2/t2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. Не использовать строки для расчета
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int inputN = new Random().Next(100, 1000);
+
+Console.WriteLine($"Изначальное число {inputN}");
+
+int newNum = (inputN / 100) * 10 + (inputN % 10);
+
+Console.WriteLine($"Новое число {newNum}");
