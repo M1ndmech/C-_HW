809dd8b23fd40931566709c20cea3abee131b56e
Mindmech <alex8008@bk.ru>
2022-12-02 19:43:49 +0300

    HW2 task 3 done

---
+++ hw2/t3/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. . Не использовать строки
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message}>");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+int inputN = Prompt("Введите число");
+int search = Prompt("Введите номер искомой цифры с конца числа");
+int count = 0;
+int length = inputN;
+
+while (length > 0)
+{
+    length = length / 10;
+    count++;
+}
+
+if (search < count)
+{
+for (int i = 0; i < (search - 1); i++)
+    {
+    inputN = inputN / 10;
+    }
+inputN = inputN % 10;
+Console.WriteLine(inputN);
+}
+else
+{
+    Console.WriteLine($"На этом месте цифры нет");
+}
