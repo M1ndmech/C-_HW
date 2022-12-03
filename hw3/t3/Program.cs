5abff02ede4d2cf579f9f5b5a8d91b6b0c85b1d3
Mindmech <alex8008@bk.ru>
2022-12-03 22:52:56 +0300

    HW3 task 3 done

---
+++ hw3/t3/Program.cs
@@ -0,0 +1,34 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message}>");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int [] GenCubeArray (int length)
+{
+    int [] array = new int [length];
+    for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = (i+1)*(i+1)*(i+1);
+        }
+    return array;
+}
+
+int number = Prompt ("Введите число");
+PrintArray(GenCubeArray(number));
