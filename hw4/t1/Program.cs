90bd726bbcbe39fe4780bad6aaebd1289d6c4fa0
Mindmech <alex8008@bk.ru>
2022-12-04 13:22:55 +0300

    HW4 task 1 done

---
+++ hw4/t1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Реализовать через функции.
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message}>");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+int Exp (int number, int exponent)
+{
+    int newNum = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        newNum = newNum * number;
+    }
+    return newNum;
+}
+
+int valA = Prompt ("Введите число");
+int valB = Prompt ("Введите степень");
+int exp = Exp (valA, valB);
+
+Console.WriteLine($"Число {valA} в степени {valB} равно {exp}");
