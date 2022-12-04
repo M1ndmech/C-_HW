e03f37101737a9036a227f4d913120963847d454
Mindmech <alex8008@bk.ru>
2022-12-04 20:59:03 +0300

    HW5 task 2 done

---
+++ hw5/t2/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "\t");
+    }
+    Console.WriteLine();
+}
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message}>");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+int [] GenArray (int length, int minRange, int maxRange)
+{
+    int [] array = new int [length];
+    for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(minRange, maxRange+1);
+        }
+    return array;
+}
+
+int SumUnevenItems (int [] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i+=2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int Range = Prompt("Введите длину массива ");
+int minValue = Prompt("Введите минимальное значение массива");
+int maxValue = Prompt("Введите максимальное значение массива");
+
+int [] arr = GenArray(Range, minValue, maxValue);
+int sumUneven = SumUnevenItems(arr);
+PrintArray (arr);
+Console.WriteLine($"Сумма элементов на нечетных позициях - {sumUneven}");
