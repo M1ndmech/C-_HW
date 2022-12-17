db8941498b8c52fe07af1857c2848e8f1de0c66d
Mindmech <alex8008@bk.ru>
2022-12-18 00:36:09 +0300

    HW8 task 2 done

---
+++ hw8/t2/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 2: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+void PrintMatrix (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+            Console.WriteLine();
+    }
+}
+
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "\t");
+    }
+    Console.WriteLine();
+}
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message}>");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+int [,] GenerateMatrix (int rows, int columns, int minRange, int maxRange)
+{
+    int [,] array = new int [rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i,j] = new Random().Next(minRange, maxRange+1);
+            }
+        }
+    return array;
+}
+
+int [] MatrixRowSums (int [,] array)
+{
+    int [] rowSums = new int [array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            rowSums[i] += array [i,j];
+        }
+    }
+    return rowSums;
+}
+
+int MaximumNumberPosition (int [] array)
+{
+    int maxPos = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > array[maxPos])
+        {
+            maxPos = i;
+        }
+    }
+    return maxPos +1;
+}
+
+int rowsInput = Prompt("Введите количество строк массива");
+int columnsInput = Prompt("Введите количество столбцов массива");
+int min = Prompt("Введите минимальное значение для ячеек массива");
+int max = Prompt("Введите максимальное значение для ячеек массива");
+
+int [,] newMatrix = GenerateMatrix(rowsInput, columnsInput, min, max);
+
+PrintMatrix (newMatrix);
+Console.WriteLine();
+PrintArray(MatrixRowSums(newMatrix));
+Console.WriteLine();
+Console.WriteLine($"Строка с максимальной суммой является {MaximumNumberPosition(MatrixRowSums(newMatrix))} в заданном массиве");
