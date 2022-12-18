d1021d19df08e53086842f3d02b15c1c8db66703
Mindmech <alex8008@bk.ru>
2022-12-18 23:04:12 +0300

    HW9 task 2 done

---
+++ hw9/t2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message}>");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+int RecursiveSumBetween (int m, int n)
+{
+    int sum = 0;
+    if (m == n + 1)
+    {
+        return sum;
+    }
+    return sum = m + RecursiveSumBetween (m + 1, n);
+}
+int start = Prompt ("Введите число, от которого считаем: ");
+int finish = Prompt ("Введите число, до которого считаем: ");
+Console.Write(RecursiveSumBetween (start, finish));
