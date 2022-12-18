f9e364f137efc3990eb698f96361c2ebda27e742
Mindmech <alex8008@bk.ru>
2022-12-18 22:47:16 +0300

    HW9 task 1 done

---
+++ hw9/t1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message}>");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+void RecursiveEvenNumbers (int m, int n)
+{
+    if (m == n + 1)
+    {
+        return;
+    }
+    if (m % 2 == 0)
+    {
+    Console.WriteLine(($"{m}; "));  
+    }
+    RecursiveEvenNumbers (m + 1, n);
+}
+int start = Prompt ("Введите число, от которого считаем: ");
+int finish = Prompt ("Введите число, до которого считаем: ");
+RecursiveEvenNumbers (start, finish);
