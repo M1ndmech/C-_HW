feacf895dc23562652dce9772b58a141c267b0b4
Mindmech <alex8008@bk.ru>
2022-12-18 23:18:58 +0300

    HW9 task 3 done

---
+++ hw9/t3/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
+
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message}>");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+int RecursiveAckerman (int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (n == 0 & m > 0)
+    {
+        return RecursiveAckerman (m - 1, 1);
+    }
+    return RecursiveAckerman (m - 1, RecursiveAckerman (m, n-1));
+}
+int a = Prompt ("Введите первый аргумент (лучше до 3): ");
+int b = Prompt ("Введите второй аргумент (также лучше до 3): ");
+Console.Write(RecursiveAckerman (a, b));
