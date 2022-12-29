7a7c1d9652aeec58aa0580ec6cf64de74574fb15
Mindmech <alex8008@bk.ru>
2022-12-29 20:58:38 +0300

    task solved

--- exam/Program.cs
+++ exam/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, из имеющегося массива строк формирующую массив из строк, длина которых меньше либо равна 3 символам.
+// Не использовать коллекции, только массивы
+
+void PrintArray (string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "\t");
+    }
+    Console.WriteLine();
+}
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message}>");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+string PromptString(string message)
+{
+    System.Console.Write($"{message}>");
+    string answer = Console.ReadLine();
+    return answer;
+}
+
+string [] GenStringArray (int length)
+{
+    string [] array = new string [length];
+    for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = PromptString($"Введите {i+1} значение:");
+        }
+    return array;
+}
+
+string [] LeaveShortValues (string [] arr, int len)
+{
+    int j = 0;
+    string [] array = new string [arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= len)
+        {
+            array[j] = arr[i];
+            j++;
+        }
+    }
+    string [] shortArray = new string [j];
+    for (int k = 0; k < j; k++)
+    {
+        shortArray[k] = array [k];
+    }
+    return shortArray;
+}
+
+int range = Prompt("Введите длину массива ");
+string [] arr = GenStringArray(range);
+PrintArray (arr);
+
+int length = Prompt("Введите максимальное количество символов в значении для его сохранения:");
+PrintArray(LeaveShortValues(arr, length));
